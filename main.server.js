6cf70e4d5a992088430acec8b84bcbfc0d187e51
Elanis <elanis@hotmail.com>
2022-01-29 15:31:08 +0100

    Refacto

--- main.server.js
+++ main.server.js
@@ -16,10 +16,44 @@ export default class NetflixCalendar {
 		});
 	}
 
+	static async insertEvent(minDate, line) {
+		const elements = line.split(',');
+
+		const start = new Date(elements[1] + ' GMT');
+		const end = new Date(elements[1] + ' GMT');
+		const duration = elements[2].split(':');
+		end.setHours(end.getHours() + parseInt(duration[0]));
+		end.setMinutes(end.getMinutes() + parseInt(duration[1]));
+		end.setSeconds(end.getSeconds() + parseInt(duration[2]));
+		const id = 'Netflix-' + start;
+
+		if(start.getTime() <= minDate.getTime()) {
+			return;
+		}
+
+		const field = {
+			id: id,
+			title: 'Netflix',
+			description: elements[4],
+			start,
+			end,
+			origin: ''
+		};
+
+		if((await Database.execQuery('SELECT id FROM calendar WHERE id = $1', [id])).rows.length === 0) {
+			const [query, values] = Database.buildInsertQuery('calendar', field);
+
+			await Database.execQuery(
+				query,
+				values
+			);
+		}
+	}
+
 	static async importNetflixActivity() {
 		const minDate = (await Database.execQuery(
-				'SELECT MAX(start) as min FROM calendar WHERE title = $1', ['Netflix']
-			)).rows[0].min;
+			'SELECT MAX(start) as min FROM calendar WHERE title = $1', ['Netflix']
+		)).rows[0].min;
 		minDate.setHours(minDate.getHours() - 6); // Safety margin
 
 		const file = fs.readFileSync(FILENAME).toString();
@@ -29,41 +63,9 @@ export default class NetflixCalendar {
 				return;
 			}
 
-			const elements = line.split(',');
-
-			const start = new Date(elements[1] + ' GMT');
-			const end = new Date(elements[1] + ' GMT');
-			const duration = elements[2].split(':');
-			end.setHours(end.getHours() + parseInt(duration[0]));
-			end.setMinutes(end.getMinutes() + parseInt(duration[1]));
-			end.setSeconds(end.getSeconds() + parseInt(duration[2]));
-			const id = 'Netflix-' + start;
-
-			if(start.getTime() <= minDate.getTime()) {
-				return;
-			}
-
-			const field = {
-				id: id,
-				title: 'Netflix',
-				description: elements[4],
-				start,
-				end,
-				origin: ''
-			};
-
-			//console.log(field);
-
-			if((await Database.execQuery('SELECT id FROM calendar WHERE id = $1', [id])).rows.length === 0) {
-				const [query, values] = Database.buildInsertQuery('calendar', field);
-
-				await Database.execQuery(
-					query,
-					values
-				);
-			}
+			await NetflixCalendar.insertEvent(minDate, line);
 		}));
 
 		log('Saved Netflix Activity', 'info');
 	}
-};
+}
